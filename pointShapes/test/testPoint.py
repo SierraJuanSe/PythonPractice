54f63c959ad5995f7a2933959c50e61cd62aac31
Sebastian Sierra <juansebastiansierrac@gmail.com>
2020-09-17 13:26:54 -0500

    Implemntacion clase punto y pruebas unitarias

---
+++ pointShapes/test/testPoint.py
@@ -0,0 +1,14 @@
+import unittest
+from src.point import Point
+
+
+class TestPoint(unittest.TestCase):
+    def setUp(self):
+        self.p1 = Point(1, 0)
+        self.p2 = Point(-1, 0)
+
+    def test_distance(self):
+        self.assertEqual(2.0, self.p1.distance(self.p2), 'Should be 2.0')
+
+    def test_inCircle(self):
+        pass
